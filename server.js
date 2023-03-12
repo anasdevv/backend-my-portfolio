b102dc7e82f6f689e2e251636eabb62a3ec03093
anasdevv <anas1.dev@gmail.com>
2023-03-12 06:50:41 +0500

    second

--- server.js
+++ server.js
@@ -2,21 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const app = express();
 const contactRoute = require('./route/contactRoute');
 app.use(express.json());
 app.use(cors());
 app.use('/contact', contactRoute);
-// app.use(express.static('client-side/build'));
-app.use(express.static(path.join(__dirname, 'client-side', 'build')));
-
-app.get('*', (req, res) => {
-  const p2 = path.join(__dirname, 'client-side', 'build', 'index.html');
-  console.log(p2);
-  res.sendFile(p2);
-});
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(process.env.PORT);
